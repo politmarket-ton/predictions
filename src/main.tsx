2ee8d88ce91ecebd60b580f8b858b22d6ed40ef3
politmarket-ton <HunterBiden228@proton.me>
2024-10-31 11:55:43 +0300

    Eruda comented

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import eruda from 'eruda';
+// import eruda from 'eruda'
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const manifestUrl = 'https://politmarket-ton.github.io/jsonstorage/tonconnect-manifest.json';
-eruda.init()
+// eruda.init()
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
